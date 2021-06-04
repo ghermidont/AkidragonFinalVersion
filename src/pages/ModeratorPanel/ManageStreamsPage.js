89b08dd71028100cf479e422b621ce7555483a6c
akaindark <akaindark@gmail.com>
2021-06-04 18:58:56 +0300

    fix add tornei and streams

--- src/pages/ModeratorPanel/ManageStreamsPage.js
+++ src/pages/ModeratorPanel/ManageStreamsPage.js
@@ -7,66 +7,72 @@ import {projectFirestore} from "../../fireBase";
 import {useStreamsContext} from "../../context/StreamsContext";
 
 const ManageStreamsPage = () => {
-    console.log("ManageStreamsPage worked");
-    const {docsFromHook} = useDataFromFirestore('streams');
-    const history = useHistory();
-    const {setChosenModifyStreamNumber} = useStreamsContext();
+  console.log("ManageStreamsPage worked");
+  const {docsFromHook} = useDataFromFirestore('streams');
+  const history = useHistory();
+  const {setChosenModifyStreamNumber} = useStreamsContext();
 
-    return (
-        <>
-            <section className='articles-page' >
-                <div className="container">
-                    <h1 className="articles-page__title title">Manage streams</h1>
-                    <Link className='btn btn__strim' to='/ModeratorAddStreamsForm'>Add streams</Link>
-                    <div className="articles-page__tab tab">
-                        <div className="articles-page__tab-body">
-                            <ul className="articles-page__tab-list active">
-                                {docsFromHook && docsFromHook.map(doc => (
-                                    <li style={{marginTop: "5em"}} className="articles-page__tab-item" key={doc.id}>
-                                        <article className='articles-page__post'>
-                                            <div>Category: {doc.category}</div>
-                                            <br/>
-                                            <div className="articles-page__image">
-                                                <img src={doc.imageURL?doc.imageURL:"https://firebasestorage.googleapis.com/v0/b/aki-dragon.appspot.com/o/articles_pictures%2Fdefault-placeholder-image.png?alt=media&token=1ead64c5-c3cc-4213-ac97-a391f8c15bf2"} alt="" className="articles-page__img"/>
-                                            </div>
-                                            <div className="articles-page__content">
-                                               <div className="articles-page__content-info">
-                                                   <ReactPlayer
-                                                       key={doc.id}
-                                                       url = {doc.videoURL}
-                                                       controls={true}
-                                                       playing={false}/>
-                                                </div>
-                                            </div>
-                                        </article>
-                                        <Link className='btn-upload'
-                                            onClick={()=> {
-                                            setChosenModifyStreamNumber(doc.id);
-                                            history.push(`/edit-stream/${doc.id}`, {from: "/ManageStreamsPage"});
-                                        }}
-                                        > Update stream >>>
-                                        </Link>
-                                        <Button
-                                          className='btn-upload btn-delete'
-                                            onClick={()=>{
-                                                projectFirestore.collection("streams").doc(doc.id).delete().then(() => {
-                                                    console.log("Document successfully deleted!");
-                                                }).catch((error) => {
-                                                    console.error("Error removing document: ", error);
-                                                });
-                                            }}
-                                        >DELETE</Button>
-                                        <br/>
-
-                                    </li>
-                                ))}
-                            </ul>
+  return (
+    <>
+      <section className='articles-page'>
+        <div className="container">
+          <h1 className="articles-page__title title">Manage streams</h1>
+          <Link className='btn btn__stream' to='/ModeratorAddStreamsForm'>Add streams</Link>
+          <div className="articles-page__tab tab">
+            <div className="articles-page__tab-body">
+              <ul className="articles-page__tab-list active">
+                {docsFromHook && docsFromHook.map(doc => (
+                  <li style={{marginTop: "5em"}} className="articles-page__tab-item stream__item" key={doc.id}>
+                    <article className='articles-page__post stream__inner'>
+                      <div className="stream__content">
+                        <h4 className='stream__title'><span>Category: </span>{doc.category}</h4>
+                        <div className="stream__image">
+                          <img
+                            src={doc.imageURL ? doc.imageURL : "https://firebasestorage.googleapis.com/v0/b/aki-dragon.appspot.com/o/articles_pictures%2Fdefault-placeholder-image.png?alt=media&token=1ead64c5-c3cc-4213-ac97-a391f8c15bf2"}
+                            alt="" className="articles-page__img"/>
                         </div>
+                      </div>
+                      <div className="stream__video">
+                        <div className="stream__player">
+                          <ReactPlayer
+                            key={doc.id}
+                            url={doc.videoURL}
+                            controls={true}
+                            playing={false}
+                            width='100%'
+                            height='100%'
+                          />
+                        </div>
+                      </div>
+                    </article>
+                    <div className="stream__box-btn">
+                      <Link className='btn-upload stream__btn'
+                            onClick={() => {
+                              setChosenModifyStreamNumber(doc.id);
+                              history.push(`/edit-stream/${doc.id}`, {from: "/ManageStreamsPage"});
+                            }}
+                      > Update stream >>>
+                      </Link>
+                      <Button
+                        className='btn-upload stream__btn'
+                        onClick={() => {
+                          projectFirestore.collection("streams").doc(doc.id).delete().then(() => {
+                            console.log("Document successfully deleted!");
+                          }).catch((error) => {
+                            console.error("Error removing document: ", error);
+                          });
+                        }}
+                      >DELETE</Button>
                     </div>
-                </div>
-            </section>
-        </>
-    );
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 
